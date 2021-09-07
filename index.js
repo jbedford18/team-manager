955eae8a719835efa66e9ed0ef595fda1b816d17
Justin B <jbedford94@gmail.com>
2021-09-06 21:29:44 -0400

    manager js file additions

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 const employeeArray = [];
 
+
+//employee information
 const createEmployee = () => {
   return inquirer.prompt([
     {
@@ -50,7 +52,7 @@ const createEmployee = () => {
     {
         type: 'input',
         name: 'school',
-        message: "",
+        message: "What school is this intern attneding?",
         when: (input) => input.role === "Intern",
     },
     {
@@ -76,3 +78,52 @@ const createEmployee = () => {
  ]);
 };
 
+//Adding in Manager details
+
+const createManager = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name? (Team Manager)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
+          }
+        }
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter the managers ID number: "
+   },
+   {
+        type: "input",
+        name: 'email',
+        message: "What is the your email?", 
+        validate: emailInput => {
+            if (emailInput) {
+              return true;
+            } else {
+                console.log ("Please enter a valid email address.");
+                return false; 
+            }
+        }
+    },
+   { type: "input",
+    name: "office",
+    message: "Enter the managers office number: "
+}
+
+    ])
+    .then(managerData => {
+        const {name, id, email, office} = managerData;
+        const manager = new Manager (name, id, email, office);
+
+        //pushes this new manager input to the array
+        employeeArray.push(manager);
+    })
+};
